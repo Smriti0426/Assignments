0b45e3d684abf1c243725d8c2c9afadc3eac9cbe
Smriti0426 <smritiagarwal260904@gmail.com>
2026-04-13 18:20:59 +0000

    assignment

---
+++ Assignment15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int i, n;
+
+    printf("How many random numbers? ");
+    scanf("%d", &n);
+
+    srand(time(0));
+
+    printf("Pseudo Random Numbers:\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d\n", rand());
+    }
+
+    return 0;
+}
