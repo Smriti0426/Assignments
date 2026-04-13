d5d935f45436c1bc2526e903a6f9b9b38dd26f8d
Smriti0426 <smritiagarwal260904@gmail.com>
2026-04-13 17:47:43 +0000

    Assignment2

---
+++ Assignment4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    float basic, gross, HRA, TA, PT, net;
+    printf("Enter basic salary : ");
+    scanf("%f",&basic);
+
+    HRA = 0.10*basic;
+    TA = 0.05*basic;
+    gross=HRA+TA+basic;
+    PT=0.02*gross;
+    net=gross-PT;
+
+    printf("HRA is %f\n",HRA);
+    printf("TA is %f\n",TA);
+    printf("Gross salary is %f\n",gross);
+    printf("Payable Tax is %f\n",PT);
+}
